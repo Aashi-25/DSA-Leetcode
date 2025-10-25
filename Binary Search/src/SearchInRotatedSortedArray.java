7c3578650ca1646292640f75c4724a01fb7c7a41
Aashi Goel <aashi25goel@gmail.com>
2025-10-25 17:29:53 +0530

    duplicate values logic in a rotated array done

--- Binary Search/src/SearchInRotatedSortedArray.java
+++ Binary Search/src/SearchInRotatedSortedArray.java
@@ -19,6 +19,7 @@ public class SearchInRotatedSortedArray {
             }
         }
     }
+    //this will not work for duplicate values
     static int FindPivot(int[] arr){
         int start = 0;
         int end = arr.length - 1;
@@ -32,6 +33,38 @@ public class SearchInRotatedSortedArray {
                 end = mid - 1;
             } else{
                 start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    static int FindPivotWithDuplicates(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start<=end){
+            int mid = start + (end-start)/2 ;
+            if (arr[mid] > arr[mid+1]){
+                return mid;
+            } else if (arr[mid-1] > arr[mid]){
+                return mid-1 ;
+            }
+            // if element at middle, start , and end are equal, then just skip the duplicates
+            else if (arr[mid] == arr[start] && arr[mid] == arr[end]){
+                //skip the duplicates after checking if they are the pivot or not
+                if(arr[start] > arr[start+1]){
+                    return start;
+                }
+                start++;
+                if(arr[end] < arr[end-1]){
+                    return end-1;
+                }
+                end--;
+            }
+            //left side is sorted so pivot should be in right
+            else if(arr[start]< arr[mid] || arr[start] == arr[mid] && arr[mid] > arr[end]){
+                start = mid + 1;
+            } else{
+                end = mid - 1;
             }
         }
         return -1;
