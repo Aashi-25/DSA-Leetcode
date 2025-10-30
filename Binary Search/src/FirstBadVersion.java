acfc32a75be8f0760c3448102d56015ced49cf36
Aashi Goel <aashi25goel@gmail.com>
2025-10-30 19:30:21 +0530

    leetcode problem 278 solved

--- Binary Search/src/FirstBadVersion.java
+++ Binary Search/src/FirstBadVersion.java
@@ -1,7 +1,7 @@
 public class FirstBadVersion {
     //You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
     static boolean isBadVersion(int n){
-
+        return false;
     }
     public int badversion(int n){
         int start = 0;
